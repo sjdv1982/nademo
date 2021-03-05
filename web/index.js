9d3cad5b962d96fa4e4ba384d3c18364ef2edd41
Sjoerd de Vries <sjdv1982@gmail.com>
2021-03-05 14:57:55 +0100

    implemented step 4 of web interface

--- web/index.js
+++ web/index.js
@@ -66,22 +66,47 @@ const app = new Vue({
   },
   watch: {
     pdb_code: function (value) {
-    seamless_update("pdb_code", value, "json")
+      seamless_update("pdb_code", value, "json")
+      loadNGL()
     },
     na_chain: function (value) {
-    seamless_update("na_chain", value, "json")
+      seamless_update("na_chain", value, "json")
+      loadNGL()
     },
     protein_chain: function (value) {
-    seamless_update("protein_chain", value, "json")
+      seamless_update("protein_chain", value, "json")
+      loadNGL()
     },
     na_resid: function (value) {
-    seamless_update("na_resid", value, "json")
+      seamless_update("na_resid", value, "json")
+      loadNGL()
     },
     protein_resid: function (value) {
-    seamless_update("protein_resid", value, "json")
+      seamless_update("protein_resid", value, "json")
+      loadNGL()
     },
-    
+
   }
 })
 
 vm = app.$mount('#app')
+
+stage = new NGL.Stage("ngl-viewer");
+// Handle window resizing
+window.addEventListener( "resize", function( event ){
+  stage.handleResize();
+}, false );
+
+function loadNGL() {
+  if (!app.pdb_code) return;
+  if (!app.na_resid) return;
+  if (!app.na_chain) return;
+  if (!app.protein_resid) return;
+  if (!app.protein_chain) return;
+  stage.removeAllComponents()
+  var pdb_url = "rcsb://" + app.pdb_code
+  var selection=`(${app.na_resid} and :${app.na_chain}) or (${app.protein_resid} and :${app.protein_chain})`
+  stage.loadFile( pdb_url, { defaultRepresentation: true } ).then( function( pdb ){
+    pdb.addRepresentation('ball+stick', {"sele":selection, "color":'blue'})
+  });
+}
